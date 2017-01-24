e26523f4669ebd7677251d004ee924a4a73337dc
Webb Peng <webb.png@gmail.com>
2017-01-24 22:21:15 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -1,14 +1,28 @@
+/* eslint-disable react/prop-types */
 
-import React from 'react';
-import { shallow } from 'enzyme';
-
-const Hello = () => <div />;
+import React, { Component } from 'react';
+import assert from 'assert';
+import Wormhole from '../src';
+import { shallow, render, mount } from 'enzyme';
 
 describe('react wormhole hoc', () => {
-	it('hello', () => {
-		const wrapper = shallow(<Hello />);
-		// console.log(typeof wrapper);
-		// console.log(Object.getOwnPropertyNames(wrapper));
-		console.log('it works.');
+	it('render', () => {
+		const val = 'hello';
+		const wormhole = new Wormhole(val);
+		const Basic = ({ a }) => (<div>{a}</div>);
+		const WrappeedBasic = wormhole.hoc('a', Basic);
+		const wrapper = render(<WrappeedBasic />);
+		assert(wrapper.find('div').text(), val);
+	});
+
+	it('update', () => {
+		const val = 'hello';
+		const updatedVal = 'world';
+		const wormhole = new Wormhole(val);
+		const Basic = ({ a }) => (<div>{a}</div>);
+		const WrappeedBasic = wormhole.hoc('a', Basic);
+		const wrapper = render(<WrappeedBasic />);
+		wormhole.set(updatedVal);
+		assert(wrapper.find('div').text(), updatedVal);
 	});
 });
